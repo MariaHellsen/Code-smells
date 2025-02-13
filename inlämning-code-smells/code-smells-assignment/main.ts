6f8036c4f1691b0fb1466ad455dd4f0066fa3f08
MariaHellsen <maria.hellsen@outlook.com>
2025-02-13 13:41:01 +0100

    Add task2

--- inlämning-code-smells/code-smells-assignment/main.ts
+++ inlämning-code-smells/code-smells-assignment/main.ts
@@ -25,6 +25,29 @@ function getLength(jumpings: number[]): number {
     2. I detta exempel har vi fokuserat på if-statements. Se om du kan göra exemplet bättre!
     */
 
+// class Student {
+//   constructor(
+//     public name: string,
+//     public handedInOnTime: boolean,
+//     public passed: boolean
+//   ) {}
+// }
+
+// function getStudentStatus(student: Student): string {
+//   student.passed =
+//     student.name == "Sebastian"
+//       ? student.handedInOnTime
+//         ? true
+//         : false
+//       : false;
+
+//   if (student.passed) {
+//     return "VG";
+//   } else {
+//     return "IG";
+//   }
+// }
+
 class Student {
   constructor(
     public name: string,
@@ -33,20 +56,10 @@ class Student {
   ) {}
 }
 
-function getStudentStatus(student: Student): string {
-  student.passed =
-    student.name == "Sebastian"
-      ? student.handedInOnTime
-        ? true
-        : false
-      : false;
-
-  if (student.passed) {
-    return "VG";
-  } else {
-    return "IG";
-  }
-}
+const getStudentStatus = (student: Student): string =>
+  (student.passed = student.name === "Sebastian" && student.handedInOnTime)
+    ? "VG"
+    : "IG";
 
 /*
     3. Variabelnamn är viktiga. Kika igenom följande kod och gör om och rätt.
